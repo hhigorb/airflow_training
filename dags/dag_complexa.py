f1634344e9248ce3af638c0c7dd4756ded3b0a87
iggy <hhigorb@outlook.com>
2023-05-06 08:32:50 -0300

    feat(dags): Adicionando mais informações sobre dags

---
+++ dags/dag_complexa.py
@@ -0,0 +1,89 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+"""
+É possível definir o trigger_rule para a DAG (assim contemplando todas as tasks) ou em uma task específica.
+"""
+
+
+doc_md = """
+Documentação da DAG
+"""
+
+dag_owner = 'data-engineer-team'
+
+default_args = {'owner': dag_owner,
+                'depends_on_past': False,
+                'retries': None,
+                'retry_delay': timedelta(minutes=5)
+                }
+
+with DAG(dag_id='dag_complexa',
+         default_args=default_args,
+         description='airflow_test_dag',
+         doc_md=doc_md,
+         start_date=datetime(year=2023, month=4, day=30),
+         schedule_interval='0 0 * * *',
+         catchup=False,
+         tags=['airflow_dag']) as dag:
+
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task2 = BashOperator(
+        task_id='task2',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task3 = BashOperator(
+        task_id='task3',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task4 = BashOperator(
+        task_id='task4',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task5 = BashOperator(
+        task_id='task5',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task6 = BashOperator(
+        task_id='task6',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task7 = BashOperator(
+        task_id='task7',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task8 = BashOperator(
+        task_id='task8',
+        bash_command="sleep 5",
+        dag=dag
+    )
+
+    task9 = BashOperator(
+        task_id='task9',
+        bash_command="sleep 5",
+        dag=dag,
+        trigger_rule='one_failed'
+    )
+
+    task1 >> task2
+    task3 >> task4
+    [task2, task4] >> task5 >> task6
+    task6 >> [task7, task8, task9]
